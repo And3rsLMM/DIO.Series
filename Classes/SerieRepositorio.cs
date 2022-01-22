4fe564d94b079544fef1dd0ff80a92cb8b255683
And3rsLMM <andersonlimam.al@gmail.com>
2022-01-22 01:41:43 -0300

    Primeiro projeto em C#

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIO.Series.Interfaces;
+using System.Collections.Generic;
+
+
+
+namespace DIO.Series
+{
+    internal class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> serieList = new List<Serie>();
+        public void DeletebyId(int id)
+        {
+            serieList[id].Delete();
+        }
+
+        public void Insert(Serie entidade)
+        {
+            serieList.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return serieList;
+        }
+
+        public int NextId()
+        {
+            return serieList.Count;
+        }
+
+        public Serie ReturnbyId(int id)
+        {
+            return serieList[id];
+        }
+
+        public void Update(int id, Serie entidade)
+        {
+                serieList[id] = entidade;
+        }
+    }
+}
---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series.Interfaces
+{
+    internal interface IRepositorio<T>
+    {
+        List<T> Lista();
+
+        T ReturnbyId(int id);
+        void Insert(T entidade);
+        void DeletebyId(int id);
+        void Update(int id, T entidade);
+        int NextId();
+    }
+}
